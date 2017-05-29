8095edb4b3dc6903e57aaa89748f4c2e5a420cf7
tjlee <tjlee@PAY-153.payletterblue.com>
2017-05-29 17:54:11 +0900

    퀵 정렬 알고리즘 수정

--- datastructure/src/sort/implementation/QuickSort.java
+++ datastructure/src/sort/implementation/QuickSort.java
@@ -5,14 +5,47 @@ package sort.implementation;
  * pivot 앞에는 pivot보다 작은 값이 오고, pivot 뒤에는 pivot보다 큰 값들이 오도록 리스트를 둘로 분할한다.
  * 분할된 두 개의 리스트에 대해 재귀함수를 통해 이 과정을 반복한다.
  * 시간복잡도 : 최악 O(n^2), 평균 O(nlogn)
- * CC : http://hahahoho5915.tistory.com/9
  */
 
 public class QuickSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arrs = { 69, 10, 30, 2, 16, 8, 31, 22 };
+	    quickSort(arrs, 0, arrs.length - 1);
+	    
+	    for (int i : arrs) {
+	        System.out.print(i + " ");
+	    }
+	}
+	
+	// 값을 비교하고 로우와 하이를 이동시키면서 값의 교환이 이루어지는 함수
+	public static int partition(int arr[], int left, int right){
+		int pivot = arr[(left + right) / 2];
+		
+		while (left < right){
+			while((arr[left] < pivot) && (left < right))
+				left++;
+			while((arr[right] > pivot) && (left < right))
+				right--;
+			
+			if(left < right){
+				int temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+			}
+		}
+		
+		return left;
+	}
+	
+	public static void quickSort(int arr[], int left, int right){
+		if(left < right){
+			// 값을 비교하고 로우와 하이를 이동시키면서 값의 교환이 이루어지는 함수
+			int pivotNewIndex = partition(arr, left, right);
+			// 퀵 정렬의 자주 나오는 '재귀', '분할' 이라고 설명되어지는 부분
+			quickSort(arr, left, pivotNewIndex - 1);
+			quickSort(arr, pivotNewIndex + 1, right);
+		}
 	}
 
 }
