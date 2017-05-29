8782fdb717142459aa0a29f4b3642e66c9867103
tjlee <tjlee@PAY-153.payletterblue.com>
2017-05-29 17:37:28 +0900

    퀵 정렬 알고리즘

---
+++ datastructure/src/sort/implementation/QuickSort.java
@@ -0,0 +1,18 @@
+package sort.implementation;
+
+/*
+ * 리스트 가운데서 하나의 원소를 고름(pivot 선정)
+ * pivot 앞에는 pivot보다 작은 값이 오고, pivot 뒤에는 pivot보다 큰 값들이 오도록 리스트를 둘로 분할한다.
+ * 분할된 두 개의 리스트에 대해 재귀함수를 통해 이 과정을 반복한다.
+ * 시간복잡도 : 최악 O(n^2), 평균 O(nlogn)
+ * CC : http://hahahoho5915.tistory.com/9
+ */
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
