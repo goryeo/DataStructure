ac10768c55a7e1621e35b2260bd857eccbf023c8
tjlee <tjlee@PAY-153.payletterblue.com>
2017-05-29 16:27:29 +0900

    삽입정렬 알고리즘

---
+++ datastructure/src/sort/implementation/InsertionSort.java
@@ -0,0 +1,29 @@
+package sort.implementation;
+
+/*
+ * 데이터를 하나씩 적절한 위치에 삽입해 가며 전체 데이터를 정렬하는 알고리즘
+ * 1. 시작은 두번째 리스트 부터 한다. 이유는 첫번째 리스트는 하나이기 때문에 정렬이 되어있다고 본다.
+ * 2. 두번째 리스트를 첫번째 리스트와 비교하여 적절한 위치에 삽입한다.
+ * 3. 새번째 리스트를 이전단계 까지 정렬되어있는 리스트에서 들어갈 위치를 찾은 후 그 곳에 삽입한다.
+ * 4. 위와 같은 방법으로 네번째 리스트 이후로 그 리스트가 끝날 때 까지 계속해서 삽입한다.
+ */
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int[] index = { 4, 1, 3, 5, 2, 6, 8, 7 };
+	    int i, j, temp, in;
+	    
+	    for(i = 1; i < index.length; i++){
+	    	in = index[i];
+	    	for(j = i - 1; (j >= 0) && (in < index[j]); j--){
+	    		index[j + 1] = index[j];
+	    	}
+	    	index[j + 1] = in;
+	    }
+	    
+	    for(i = 0; i < index.length; i++){
+	    	System.out.print(index[i] + " ");
+	    }
+	}
+}
--- datastructure/src/sort/implementation/SelectionSort.java
+++ datastructure/src/sort/implementation/SelectionSort.java
@@ -1,10 +1,12 @@
 package sort.implementation;
+
 /*
 * 1. 주어진 리스트 중에 최소값을 찾는다.
 * 2. 그 값을 맨 앞에 위치한 값과 교체한다.
 * 3. 맨 처음 위치를 뺀 나머지 리스트를 같은 방법으로 교체한다.
 * 처음부터 끝까지 조사하면서 최솟값(또는 최댓값)을 선택하여 정렬하는 알고리즘
 */
+
 public class SelectionSort {
   public static void main(String[] args) {
     int[] index = { 5, 4, 7, 1, 8, 3, 2, 6 };
